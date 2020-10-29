ffe0d3854aed740cdf17a9345de9ac8fc9340a55
Aidan Duff <aidanpatrickduff@gmail.com>
2020-10-29 11:48:18 +0000

    style updates

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -4,11 +4,12 @@ import Logo from './logo';
 
 const Jumbotron = () => {
     return ( 
-        <div className="jumbotron" style={{ backgroundColor: "#f88642" }}>
+        <div className="jumbotron " style={{ backgroundColor: "#f88642" }}>
         <div className="container">
-          <Logo />
-          <h1 className="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p className="row"><Logo /></p>
+          {/* <h1 className="display-3 row">Hello, world!</h1> */}
+          <h2>Welcome to Skweezit!</h2>
+          <h4>Enter your long link in the field below and hit the skweez button to obtain a shortened link.</h4><br></br>
           <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
         </div>
       </div>
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from "../images/logo.png";
 
 const Logo = () => {
-    return ( <img src={logo} alt="logo" class="img-fluid rounded mx-auto d-block m-2"></img> );
+    return ( <img src={logo} alt="logo" class="img-fluid rounded  d-block m-2"></img> );
 }
  
 export default Logo;
