f95a6dc75167bf3582d6f1e37233736bf20743b4
Aidan Duff <aidanpatrickduff@gmail.com>
2020-10-30 13:33:28 +0000

    took out 'learn more' button for now

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -10,7 +10,7 @@ const Jumbotron = () => {
           {/* <h1 className="display-3 row">Hello, world!</h1> */}
           <h2>Welcome to Skweezit!</h2>
           <h4>Enter your long link in the field below and hit the skweez button to obtain a shortened link.</h4><br></br>
-          <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          {/* <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p> */}
         </div>
       </div>
      );
