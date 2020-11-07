b62761de8591f089747cf93a877846c322cdd92c
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-07 10:25:33 +0000

    some refactoring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,6 @@ const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a className="navbar-brand" href="#">Skweezit</a>
-    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button> */}
     <div className="collapse navbar-collapse" id="navbarCollapse">
       <ul className="navbar-nav mr-auto">
       <li className="nav-item">
