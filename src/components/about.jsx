dd946bb833234c52d3a9669fa39fee27e0591ee0
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-07 11:23:23 +0000

    added icons for github, Linkedin etc

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,22 @@ const About = () => {
         
         <h4>Other examples of my work are available to demo at gitshowcase.com/aidanduff.</h4><br></br>
         
-        <h4>For any queries or comoplaints, please email me at aidanduff@gmail.com.</h4></div> );
+        <h4>For any queries or complaints, please email me at aidanduff@gmail.com.
+        </h4><br></br>
+        
+        <p className="text-center">
+          <a href="https://github.com/aidanduff" className="text-right">
+            <i className="fa fa-github fa-github-about  fa-2x" style={{color: "#50381b"}}> </i>
+          </a>
+          &nbsp;
+          <a
+            href="https://www.linkedin.com/in/aidan-d-31294a16a/"
+            className="text-right"
+          >
+            <i className="fa fa-linkedin fa-linkedin-about fa-2x" style={{color: "#50381b"}}> </i>
+          </a>
+        </p>
+        </div> );
 }
  
 export default About;
