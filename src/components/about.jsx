563ab7c0fcc2ac57d55549805542e14762edb63c
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-07 10:36:22 +0000

    added about text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const About = () => {
-    return ( <h1>About</h1> );
+    return ( <div className="container text-center"><h4>Created by Aidan Duff in 2020.</h4><br></br>
+
+        <h4>As this is a demo project which utilizeses a free hosting service, shortened links are not as short as other link shortening services. This is due to the herokuapp prefix. </h4><br></br>
+        
+        <h4>Other examples of my work are available to demo at gitshowcase.com/aidanduff.</h4><br></br>
+        
+        <h4>For any queries or comoplaints, please email me at aidanduff@gmail.com.</h4></div> );
 }
  
 export default About;
