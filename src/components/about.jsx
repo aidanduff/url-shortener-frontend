d15942269257c7e884b49b93dea1daf62c44059e
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-08 21:48:03 +0000

    someo changes in about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const About = () => {
-    return ( <div className="container text-center"><h4>Created by Aidan Duff in 2020.</h4><br></br>
+    return ( <div className="container text-center"><h4>Skweezit v1.0.0</h4><br></br>
 
         <h4>As this is a demo project which utilizeses a free hosting service, shortened links are not as short as other link shortening services. This is due to the herokuapp prefix. </h4><br></br>
         
-        <h4>Other examples of my work are available to demo at gitshowcase.com/aidanduff.</h4><br></br>
+        <h4>Other examples of my work are available to demo at <a href="https://www.gitshowcase.com/aidanduff">gitshowcase.com/aidanduff</a></h4><br></br>
         
         <h4>For any queries or complaints, please email me at aidanduff@gmail.com.
         </h4><br></br>
