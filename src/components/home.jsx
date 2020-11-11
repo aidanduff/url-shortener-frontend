2ff8b96ebe03e44373a5327371f2b570da39311d
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-11 10:10:39 +0000

    update URL

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -19,7 +19,7 @@ class Home extends Component {
             },
           };
 
-          const response = await http.post('https://skweezit-server.herokuapp.com/skweez.it', string, config);
+          const response = await http.post('https://skweezit-server.herokuapp.com/home', string, config);
           this.setState({shortenedUrl: response.data.shortenedURL });
       };
 
