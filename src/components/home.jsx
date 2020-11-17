c796698dff055989a76bc13b6ca5fdcb4f074912
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-17 13:51:37 +0000

    trying out non async/await server wake

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,7 +11,7 @@ class Home extends Component {
     componentDidMount(){
         http.get('https://skweezit-server.herokuapp.com/').then((response) =>
             {
-                console.log("response");
+                console.log(response);
             });
     }
 
