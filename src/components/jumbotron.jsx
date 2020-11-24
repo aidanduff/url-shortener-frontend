7e71fadf83230cd2d5fba5966b0f96372576aa9b
Aidan Duff <aidanpatrickduff@gmail.com>
2020-11-24 13:56:34 +0000

    cleaned up jumbotron

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -7,10 +7,8 @@ const Jumbotron = () => {
         <div className="jumbotron " style={{ backgroundColor: "#f88642" }}>
         <div className="container mt-4">
           <p className="row"><Logo /></p>
-          {/* <h1 className="display-3 row">Hello, world!</h1> */}
           <h2>Welcome to Skweezit!</h2>
           <h4>Enter your long link in the field below and hit the skweez button to obtain a shortened link.</h4><br></br>
-          {/* <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p> */}
         </div>
       </div>
      );
